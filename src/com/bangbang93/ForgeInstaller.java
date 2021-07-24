0b898ba8a11938417dd7a23c796c62d8d31ad947
bangbang93 <bangbang93@163.com>
2021-07-24 12:43:05 +0800

    支持新版forge

--- src/com/bangbang93/ForgeInstaller.java
+++ src/com/bangbang93/ForgeInstaller.java
@@ -4,15 +4,24 @@ import net.minecraftforge.installer.SimpleInstaller;
 import net.minecraftforge.installer.actions.ActionCanceledException;
 import net.minecraftforge.installer.actions.ClientInstall;
 import net.minecraftforge.installer.actions.ProgressCallback;
-import net.minecraftforge.installer.json.Install;
 import net.minecraftforge.installer.json.Util;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.function.Predicate;
 
 public class ForgeInstaller {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
+    InvocationTargetException, InstantiationException, IllegalAccessException {
     SimpleInstaller.headless = true;
-    Install install = Util.loadInstallProfile();
+    Class<?> installerClass;
+    try {
+     installerClass = Class.forName("net.minecraftforge.installer.json.InstallV1");
+    } catch (ClassNotFoundException e) {
+        installerClass = Class.forName("net.minecraftforge.installer.json.Install");
+    }
+    Object install = Util.class.getDeclaredMethod("loadInstallProfile").invoke(Util.class);
     ProgressCallback monitor = ProgressCallback.withOutputs(System.out);
     String path;
     if (args.length == 0) {
@@ -20,12 +29,21 @@ public class ForgeInstaller {
     } else {
       path = args[0];
     }
-    ClientInstall action = new ClientInstall(install, monitor);
-    try {
-      boolean result = action.run(new File(path), (a) -> true);
-      monitor.message(Boolean.toString(result));
-    } catch (ActionCanceledException e) {
-      e.printStackTrace();
+    ClientInstall action = (ClientInstall) Class.forName("net.minecraftforge.installer.actions.ClientInstall")
+      .getConstructor(installerClass, ProgressCallback.class).newInstance(install, monitor);
+    Method[] methods = ClientInstall.class.getMethods();
+    Predicate<String> optionals = (a) -> true;
+    Object result = null;
+    for (Method method : methods) {
+      if (method.getName().equals("run")) {
+        if (method.getParameters().length == 2) {
+          result = method.invoke(action, new File(path), optionals);
+        } else {
+          String p = SimpleInstaller.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+          result = method.invoke(action, new File(path), optionals, new File(p));
+        }
+      }
     }
+    monitor.message(result != null ? result.toString() : null);
   }
 }
